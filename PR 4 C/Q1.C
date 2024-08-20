c39540bd69b52b05e92f64af741ed8385d3068f8
RONIT JARIWALA <ronitjariwala2006@gmail.com>
2024-08-20 11:38:10 +0530

    Add files via upload

---
+++ PR 4 C/Q1.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	
+
+int i,j,num=11;
+
+for(i=1; i<=5; i++)
+{
+	num=41;
+	for(j=1; j<=i; j++)
+	{
+		printf("%d ",num);
+		num++;
+	}
+	printf("\n");
+}
+
+}
